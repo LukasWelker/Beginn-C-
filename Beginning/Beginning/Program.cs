5ef8ff26745847d145d7e23dbd14c0eb1b67ade1
Welker, Lukas <lukas.welker@tecalliance.de>
2022-09-15 09:27:16 +0200

    we aks user for his age and print it out #4

--- Beginning/Beginning/Program.cs
+++ Beginning/Beginning/Program.cs
@@ -12,7 +12,13 @@ namespace Beginning
         {
             Console.WriteLine("Wie heißt du ?");
             string name = Console.ReadLine();
+
             Console.WriteLine("Hello " + name);
+
+            Console.WriteLine("Wie alt bist du ?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Du bist also " + age);
+
             Console.ReadLine();
           
         }
