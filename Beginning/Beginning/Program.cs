d4b1978010c27c3ee50f824123c189eb481aed0d
Welker, Lukas <lukas.welker@tecalliance.de>
2022-09-15 10:13:33 +0200

    Created second condition if you are younger than 28 #8

--- Beginning/Beginning/Program.cs
+++ Beginning/Beginning/Program.cs
@@ -12,13 +12,17 @@ namespace Beginning
         {
             Console.WriteLine("Wie alt bist du?");
             int age = Convert.ToInt32(Console.ReadLine());
-            if(age <18)
+            if(age < 18)
             {
                 Console.WriteLine("Du kommst hier nicht rein!");
             }
+            else if (age < 28)
+            {
+                Console.WriteLine("Bitte ihren Ausweis zeigen");
+            }
             else
             {
-                Console.WriteLine("´Viel Spaß auf der Feier!!");          
+                Console.WriteLine("Viel Spaß auf der Feier!!");          
             }
             Console.ReadLine();
         }
