0076733e30a74ee8e03f7f87902153efe7d7fdb0
Welker, Lukas <lukas.welker@tecalliance.de>
2022-09-15 11:02:30 +0200

    Do mindestens 1mal while wird eventuell gar nicht ausgehführt

--- Beginning/Beginning/Program.cs
+++ Beginning/Beginning/Program.cs
@@ -11,12 +11,20 @@ namespace Beginning
         static void Main(string[] args)
         {
             int i = 0;
-            while (i < 10)
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            } while (i < 10);
+
+            while (i < 20)
             {
                 Console.WriteLine(i);
                 i++;
             }
-         
+
+
+                
 
             Console.ReadLine();
         }
